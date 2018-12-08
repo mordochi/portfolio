d48b7d3ab20c09ce4d6c52be8d7883d76d34345a
mordochi <lilynnchi@gmail.com>
2018-12-08 17:10:27 +0800

    add nav and about me

---
+++ index.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  const aboutMe = document.getElementById("about-me");
+  const projects = document.getElementById("projects");
+
+  projects.style.display = "none";
+};
+
+function changeTab(e, id) {
+  const tabContents = document.querySelectorAll(".tab-content");
+  for(let i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+  }
+
+  const showDiv = document.getElementById(id);
+  showDiv.style.display = "block";
+
+  const tabs = document.querySelectorAll("nav p");
+  for(let i = 0; i < tabs.length; i++) {
+    if(tabs[i].classList.contains('active')) {
+      tabs[i].classList.remove('active');
+    }
+  }
+
+  e.currentTarget.className = "active";
+}
